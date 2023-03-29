87b80405b21be94a2736283c70a82792de23d11d
MichiCaballero07 <michelcaballerogranado@gmail.com>
2023-03-29 11:09:06 -0700

    Update _printf function

--- _printf.c
+++ _printf.c
@@ -12,52 +12,43 @@ int _printf(const char *format, ...)
 	va_list arg;
 	const char *p;
 	int n = 0;
-
-	if (format == NULL)
-		return (-1);
-	va_start(arg, format);
-	for (p = format; *p; p++)
-	{
-		if (*p == '%' && *p + 1 != '%')
-		{
-			witch(++p, arg, &n);
-		}
-		else
-			_putchar(*p), n++;
-	}
-	va_end(arg);
-	return (n);
-}
-
-/**
- * witch - Funtion
- *
- * @p: Pointer to a string(format)
- * @arg: variadic list
- * @n: counter
- */
-void witch(const char *p, va_list arg, int *n)
+if (format == NULL)
+return (-1);
+va_start(arg, format);
+for (p = format; *p; p++)
 {
-	*n += 1;
-	switch (*p)
-	{
-		case 's':
-			*n += print_string(arg);
-			break;
-		case 'c':
-			*n += print_char(arg);
-			break;
-		case '%':
-			_putchar('%'), *n++;
-			break;
-		case 'u':
-			*n += print_unsigned_int(arg);
-			break;
-		case 'i':
-		case 'd':
-			*n += print_int(arg);
-			break;
-		default:
-			_putchar('%'), _putchar(*p), *n += 2;
-	}
+if (*p == '%' && *p + 1 == '%')
+{
+_putchar(*p), n++;
+continue;
 }
+else if (*p == '%' && *p + 1 != '%')
+{
+switch (*++p)
+{
+case 's':
+n += print_string(arg);
+break;
+case 'c':
+n += print_char(arg);
+break;
+case '%':
+_putchar('%'), n++;
+break;
+case '\0':
+return (-1);
+case 'i':
+case 'd':
+n += print_int(arg);
+break;
+default:
+_putchar('%'), _putchar(*p), n += 2;
+}
+}
+else
+_putchar(*p), n++;
+}
+va_end(arg);
+return (n);
+}
+
